a55b53674403c2ed017092837b15c8a0fecce278
robots4life <34193243+robots4life@users.noreply.github.com>
2022-03-23 16:17:59 +0100

    :sparkles:

---
+++ src/routes/index.svelte
@@ -0,0 +1,4 @@
+<h1 class="text-4xl py-4">Welcome to SvelteKit</h1>
+<p class="text-2xl py-3">
+	Visit <a class="text-red-400" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
---
+++ svelte.config.js
@@ -0,0 +1,24 @@
+import adapter from '@sveltejs/adapter-auto';
+
+// import adapter from '@sveltejs/adapter-node';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+		// https://github.com/sveltejs/kit/tree/master/packages/adapter-node
+		// adapter: adapter({ out: 'build' }),
+		vite: {
+			server: {
+				host: '0.0.0.0',
+				port: 3000
+			},
+			define: {
+				// env-cmd https://blog.hdks.org/Environment-Variables-in-SvelteKit-and-Vercel/
+				'process.env': process.env
+			}
+		}
+	}
+};
+
+export default config;
