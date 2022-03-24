4cef80280a74fa720a7e48eeb3a3eac91bddb4ab
robots4life <34193243+robots4life@users.noreply.github.com>
2022-03-24 14:40:55 +0100

    setup supabase auth functions

---
+++ src/lib/services.js
@@ -0,0 +1,19 @@
+import supabase from './supabase';
+
+// https://supabase.com/docs/reference/javascript/auth-user
+// Inside a browser context, user() will return the user data, if there is a logged in user.
+export function getUserData() {
+	return supabase.auth.user();
+}
+
+// https://supabase.com/docs/reference/javascript/auth-signin
+export async function signIn({ email }) {
+	const { user, session, error } = await supabase.auth.signIn({ email });
+	return { data: !error, error };
+}
+
+// https://supabase.com/docs/reference/javascript/auth-signout
+export async function signOut() {
+	const { error } = await supabase.auth.signOut();
+	return { data: !error, error };
+}
